ffc218c73788d72e047fcf1f615f10658ef8be08
Mritunjay Kumar <144582396+MritunjayKumar07@users.noreply.github.com>
2023-11-23 18:33:14 +0530

    Update Footer.js
    Change Url

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
                     <li style={{listStyle:"none", padding:"8px", background:"none"}}><a href='https://twitter.com/Promocrux' target="_blank"><i style={{color:"#FFCACA", background:"none", fontSize:"28px"}} class='bx bxl-twitter'></i></a></li>
                     <li style={{listStyle:"none", padding:"8px", background:"none"}}><a href='https://www.instagram.com/promocrux/' target="_blank"><i style={{color:"#FFCACA", background:"none", fontSize:"28px"}} class='bx bxl-instagram-alt' ></i></a></li>
                     <li style={{listStyle:"none", padding:"8px", background:"none"}}><a href='https://www.youtube.com/channel/UCzGDWcS-LAn-2eqnjp6BvqQ' target="_blank"><i style={{color:"#FFCACA", background:"none", fontSize:"28px"}} class='bx bxl-youtube'></i></a></li>
-                    <li style={{listStyle:"none", padding:"8px", background:"none"}}><a href='https://github.com/MritunjayKumarStudent' target="_blank"><i style={{color:"#FFCACA", background:"none", fontSize:"28px"}} class='bx bxl-github'></i></a></li>
+                    <li style={{listStyle:"none", padding:"8px", background:"none"}}><a href='https://github.com/MritunjayKumar07' target="_blank"><i style={{color:"#FFCACA", background:"none", fontSize:"28px"}} class='bx bxl-github'></i></a></li>
                     <li style={{listStyle:"none", padding:"8px", background:"none"}}><a href='https://www.linkedin.com/in/promocrux-84a81225b/' target="_blank"><i style={{color:"#FFCACA", background:"none", fontSize:"28px"}} class='bx bxl-linkedin-square' ></i></a></li>
                 </ul>
             </div>
